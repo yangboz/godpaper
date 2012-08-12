3973a18399bc2b55c19a05a1942ed58ca19d508d
Knight.zhou <YoungWelle@gmail.com>
2012-08-12 23:19:22 +0800

    CHANGED:apply the BitVector.h with functions and implement functions at .m file;(TBC)

--- TheRealBishop/TheRealBishop/GP_BitVector.h
+++ TheRealBishop/TheRealBishop/GP_BitVector.h
@@ -9,5 +9,40 @@
 #import <Foundation/Foundation.h>
 
 @interface GP_BitVector : NSObject
+{
+//    private var _bits:Array;
+//    private var _arrSize:int;
+//    private var _bitSize:int;
+    @private
+        NSMutableArray *_bits;
+        int _arrSize,_bitSize;
+}
+//Methods
+//public function BitVector(bits:int)
+-(GP_BitVector *)BitVector:(int)bits;
+
+//public function get bitCount():int
+@property(readonly,assign) int bitCount;
+
+//public function get cellCount():int
+@property(readonly,assign) int cellCount;
+
+//public function getBit(index:int):int
+-(int)getBit:(int)index;
+
+//public function setBit(index:int, b:Boolean):void
+-(void)setBit:(int)index boolValue:(BOOL)b;
+
+//public function resize(size:int):void
+-(void)resize:(int)size;
+
+//public function clear():void
+-(void)clear;
+
+//public function setAll():void
+-(void)setAll;
+
+//public function toString():String
+-(NSString *)toString;
 
 @end
--- TheRealBishop/TheRealBishop/GP_BitVector.m
+++ TheRealBishop/TheRealBishop/GP_BitVector.m
@@ -9,5 +9,135 @@
 #import "GP_BitVector.h"
 
 @implementation GP_BitVector
+/**
+ * Creates a bit-vector with a given number of bits. Each cell holds an
+ * int, allowing to store 32 flags each. 
+ * 
+ * @param bits The total number of bits.
+ */
+//public function BitVector(bits:int)
+-(GP_BitVector *)BitVector:(int)bits
+{
+//    _bits = [];
+//    _arrSize = 0;
+//    
+//    resize(bits);
+    _bits = [[NSMutableArray alloc] init];
+    _arrSize = 0;
+    //
+    [self resize:bits];
+    return self;
+}
+
+/**
+ * The total number of bits.
+ */
+//public function get bitCount():int
+-(int)bitCount
+{
+    return _arrSize * 31;
+}
+
+/**
+ * The total number of cells.
+ */
+//public function get cellCount():int
+-(int)cellCount
+{
+    return _arrSize;
+}
+
+/**
+ * Gets a bit from a given index.
+ * 
+ * @param index The index of the bit.
+ */
+//public function getBit(index:int):int
+-(int)getBit:(int)index
+{
+//    var bit:int = index % 31;
+//    return (_bits[(index / 31) >> 0] & (1 << bit)) >> bit;
+    int bit = index % 31;
+    return ((int)[_bits objectAtIndex:((index /31) >>0)] & (1 << bit) ) >> bit;
+}
+
+/**
+ * Sets a bit at a given index.
+ * 
+ * @param index The index of the bit.
+ * @param b     The boolean flag to set.
+ */
+//public function setBit(index:int, b:Boolean):void
+-(void)setBit:(int)index boolValue:(BOOL)b
+{
+//    var cell:int = index / 31;
+//    var mask:int = 1 << index % 31;
+//    _bits[cell] = b ? (_bits[cell] | mask) : (_bits[cell] & (~mask));
+}
+
+/**
+ * Resizes the bit-vector to an appropriate number of bits.
+ * 
+ * @param size The total number of bits.
+ */
+//public function resize(size:int):void
+-(void)resize:(int)size
+{
+//    if (size == _bitSize) return;
+//    _bitSize = size;
+//    
+//    //convert the bit-size to integer-size
+//    if (size % 31 == 0)
+//        size /= 31;
+//    else
+//        size = (size / 31) + 1;
+//    
+//    if (size < _arrSize)
+//    {
+//        _bits.splice(size);
+//        _arrSize = size;
+//    }
+//    else
+//    {
+//        _bits = _bits.concat(new Array(size - _arrSize));
+//        _arrSize = _bits.length;
+//    }
+}
+
+/**
+ * Resets all bits to 0;
+ */
+//public function clear():void
+-(void)clear
+{
+//    var k:int = _bits.length;
+//    for (var i:int = 0; i < k; i++)
+//        _bits[i] = 0;
+}
+
+/**
+ * Sets each bit to 1.
+ */
+//public function setAll():void
+-(void)setAll
+{
+//    var k:int = _bits.length;
+//    for (var i:int = 0; i < k; i++)
+//        _bits[i] = int.MAX_VALUE;
+}
+
+/**
+ * Prints out a string representing the current object.
+ * 
+ * @return A string representing the current object.
+ */
+//public function toString():String
+-(NSString *)toString
+{
+//return "[BitVector, size=" + _bitSize + "]";
+    NSString *str = @"[BitVector, size=";
+    str = [NSString stringWithFormat:@"%i %@",_bitSize,@"]"];
+    return str;
+}
 
 @end
